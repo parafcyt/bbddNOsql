aad40e66ee949706c79a5158aa9f430f5eb8f1ae
parafcyt <cuentafacultadgv@gmail.com>
2020-10-12 17:26:03 -0300

    cliente terminado

--- TP2 Node JS y Redis/TP2 parte 2-Tickets/codigo/servidor/servidor.py
+++ TP2 Node JS y Redis/TP2 parte 2-Tickets/codigo/servidor/servidor.py
@@ -1,7 +1,8 @@
 
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
+import os
 from rutas import ruta
 
 app= Flask(__name__)
@@ -10,6 +11,27 @@ app= Flask(__name__)
 cors=CORS(app,resources={r"/*": {"origins":"*"}}) #"*" para que cualquier ruta se conecte
 app.register_blueprint(ruta)
 
+# ANGULAR ROUTING
+__angular_paths = []
+__angular_default_path = "index.html"
+__root = "./cliente/"
+
+for root, subdirs, files in os.walk(__root):
+    if len(root) > len(__root):
+        root += "/"
+
+    for file in files:
+        relativePath = str.replace(root + file, __root, "")
+        __angular_paths.append(relativePath)
+
+# Special trick to capture all remaining routes
+@app.route('/<path:path>')
+@app.route('/', defaults={'path': ''})
+def angular(path):    
+    if path not in __angular_paths:
+        path = __angular_default_path
+    
+    return send_from_directory('./cliente/', path)
 
 
 #lo que se ejecuta si este archivo es el main(principal)
